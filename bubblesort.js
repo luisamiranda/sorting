4b56c6d5b18de22f637973d935cb56198b39b337
Luisa de Miranda <lemiranda@gmail.com>
2017-01-12 16:05:40 -0500

    additions to bubbleSort and bubbleSort spec

--- bubblesort.js
+++ bubblesort.js
@@ -1,7 +1,25 @@
 function bubbleSort(array) {
-	var elem1, elem2;
-	for(var i = 0; i < array.length; i++){
-		elem1 = array[i];
-		elem2 = array[i+1];
+	if (array.length < 2) {
+		return array;
 	}
+	for(var i = 0; i < array.length-1; i++){
+		var elem1 = array[i];
+	}
+	for (var j = 1; j < array.length; j++) {
+			var elem2 = array[j]
+			if (elem1 > elem2) {
+				array = swap(i, j, array);
+			}
+	}
+	if (array == array.sort()) {
+		return array
+	}
+}
+
+function swap(index, index2, array){
+	var temp = array[index];
+	array[index] = array[index2];
+	array[index2] = temp;
+
+	return array;
 }
--- bubblesort.spec.js
+++ bubblesort.spec.js
@@ -1,6 +1,11 @@
-before(function(){
-	spyOn('bubbleSort, 'swap'').and.callThrough();
+beforeEach(function(){
+		spyOn(window, "swap").and.callThrough();
+ });
+it('involves exactly array.length * array.length calls', function () {
+  bubbleSort();
+  expect(window.swap.calls.count()).toEqual(9);
 });
+
 describe('Bubble Sort', function(){
   it('handles an empty array', function(){
     expect(bubbleSort([])).toEqual([]);
@@ -8,10 +13,10 @@ describe('Bubble Sort', function(){
   it('handles an array with one element', function(){
   	expect(bubbleSort([1])).toEqual([1]);
   });
-  it('sorts and array with multitple elements', function(){
-  	expect(bubbleSort([2,1,3]).toEqual([1,2,3]));
+  it('sorts an array with multiple elements', function(){
+  	expect(bubbleSort([2,1,3])).toEqual([1,2,3]);
   });
   it('sorts an array with duplicate elements', function(){
-  	expect(bubbleSort([4.2.3.1.5,4]).toEqual([1,2,3,4,4,5]));
+  	expect(bubbleSort([4,2,3,1,4])).toEqual([1,2,3,4,4]);
   });
 });
